0825232478290915781a4856a8c71ee4310bdd43
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-08-04 12:36:21 -0500

    Change PROJECT_ID to DESCOPE_PROJECT_ID

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { AuthProvider } from '@descope/react-sdk'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider projectId={process.env.REACT_APP_PROJECT_ID}>
+    <AuthProvider projectId={process.env.REACT_APP_DESCOPE_PROJECT_ID}>
       <App />
     </AuthProvider>
   </React.StrictMode>
--- server/app.py
+++ server/app.py
@@ -4,46 +4,48 @@ from descope import DescopeClient
 from functools import wraps
 
 
-app = Flask(__name__) # initialize flask app
+app = Flask(__name__)  # initialize flask app
 
 
 try:
-    descope_client = DescopeClient(project_id=os.environ.get("PROJECT_ID")) # initialize the descope client
+    descope_client = DescopeClient(project_id=os.environ.get(
+        "DESCOPE_PROJECT_ID"))  # initialize the descope client
 except Exception as error:
-    print ("failed to initialize. Error:")
-    print (error)
+    print("failed to initialize. Error:")
+    print(error)
 
 
-def token_required(f): # auth decorator
+def token_required(f):  # auth decorator
     @wraps(f)
     def decorator(*args, **kwargs):
         session_token = None
 
-        if 'Authorization' in request.headers: # check if token in request
+        if 'Authorization' in request.headers:  # check if token in request
             auth_request = request.headers['Authorization']
             session_token = auth_request.replace('Bearer ', '')
-        if not session_token: # throw error
+        if not session_token:  # throw error
             return make_response(jsonify({"error": "❌ invalid session token!"}), 401)
 
-        try: # validate token
-            jwt_response = descope_client.validate_session(session_token=session_token)
+        try:  # validate token
+            jwt_response = descope_client.validate_session(
+                session_token=session_token)
         except:
             return make_response(jsonify({"error": "❌ invalid session token!"}), 401)
 
         return f(jwt_response, *args, **kwargs)
 
     return decorator
-  
 
-@app.route('/get_roles', methods=['GET']) 
+
+@app.route('/get_roles', methods=['GET'])
 @token_required
 def get_roles(jwt_response):
-    roles = jwt_response["roles"] 
+    roles = jwt_response["roles"]
 
-    return { "secretMessage": "You are now a trained Descope user!", "roles": roles}
-  
+    return {"secretMessage": "You are now a trained Descope user!", "roles": roles}
 
-@app.route('/get_role_data', methods=['GET']) 
+
+@app.route('/get_role_data', methods=['GET'])
 @token_required
 def get_role_data(jwt_response):
     valid_student_role = descope_client.validate_roles(
