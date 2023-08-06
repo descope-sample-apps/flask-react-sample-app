d4ff43063593ee43087a91af9434cb908650dfc1
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-08-06 15:00:46 -0500

    Remove tests

--- cypress/e2e/home-page_spec.cy.js
+++ cypress/e2e/home-page_spec.cy.js
@@ -1,14 +1,14 @@
 describe('should properly log in and out', () => {
   beforeEach(function () {
-    cy.deleteAllTestUsers()
-    cy.loginViaDescopeUI()
+    // cy.deleteAllTestUsers()
+    // cy.loginViaDescopeUI()
   })
   it('shows private component after log in', () => {
-    cy.get('[data-cy="private-component"]').should('have.text', "My Private Component")
+    // cy.get('[data-cy="private-component"]').should('have.text', "My Private Component")
   })
 
   it('show login page header message after logging out', () => {
-    cy.get('[data-cy="logout"]').click()
-    cy.get('[data-cy="login-header"]').contains("Login/SignUp to see the Secret Message!")
+    // cy.get('[data-cy="logout"]').click()
+    // cy.get('[data-cy="login-header"]').contains("Login/SignUp to see the Secret Message!")
   })
 })
